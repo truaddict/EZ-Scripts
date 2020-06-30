c062b027f6ab5c8abac8be97b51f2269e537eeec
truaddict <480471+truaddict@users.noreply.github.com>
2020-07-01 02:21:51 +0300

    russian translation, changed rewards

--- scripts/mobReward.js
+++ scripts/mobReward.js
@@ -27,33 +27,43 @@ system.listenForEvent("minecraft:entity_death", ({data: eventData}) => {
 	//console.log(JSON.stringify(deadEntity));
 	
 	const mobs = {
-		"zombie" : randomReward(1,1),
-		"zombie_villager_v2" : randomReward(1,1),
-		"husk" : randomReward(1,1),
-		"skeleton" : randomReward(1,1),
-		"stray" : randomReward(1,1),
-		"creeper" : randomReward(1,1),
-		"spider" : randomReward(1,1),
-		"cave_spider" : randomReward(1,1),
-		"witch" : randomReward(2,3),
-		"phantom" : randomReward(1,2),
-		"blaze" : randomReward(2,2),
-		"ghast" : randomReward(3,3),
-		"ender_dragon" : randomReward(200,200),
-		"piglin" : randomReward(1,3),
-		"zoglin" : randomReward(2,5)
+		//"zombie" : randomReward(1,1),
+		//"zombie_villager_v2" : randomReward(1,1),
+		//"husk" : randomReward(1,1),
+		//"skeleton" : randomReward(1,1),
+		"stray" : randomReward(2,2),
+		"creeper" : randomReward(3,3),
+		//"spider" : randomReward(1,1),
+		//"cave_spider" : randomReward(1,1),
+		"witch" : randomReward(5,5),
+		//"phantom" : randomReward(1,2),
+		//"blaze" : randomReward(2,2),
+		"ghast" : randomReward(2,2),
+		"ender_dragon" : randomReward(800,800),
+		"piglin" : randomReward(3,3),
+		"zoglin" : randomReward(4,4),
+		"elder_guardian" : randomReward(100,100),
+		"endermite" : randomReward(13,13),
+		"evoker" : randomReward(5,5),
+		"pillager" : randomReward(3,3),
+		"ravager" : randomReward(5,5),
+		"shulker" : randomReward(10,10),
+		"vex" : randomReward(5,5),
+		"vindicator" : randomReward(5,5),
+		"wither" : randomReward(500,500),
+		"wither_skeleton" : randomReward(4,4)
 	};
 	
 	if (deadEntityIdentifier in mobs) {
 		let moneyOld = getBalance(player)
 		updateBalance(player, mobs[deadEntityIdentifier], "add");
-		system.executeCommand(`title "${player.name}" actionbar §fYou earned §e$${mobs[deadEntityIdentifier]} §ffor killing §6${deadEntity.__identifier__}\n§7Previous: §b${moneyOld} §7=> Now: §b${getBalance(player)}`, () => {});
+		system.executeCommand(`title "${player.name}" actionbar §Вы заработали §e$${mobs[deadEntityIdentifier]} §fза убийство §6${deadEntity.__identifier__}\n§7Было: §b${moneyOld} §7=> Стало: §b${getBalance(player)}`, () => {});
 		//console.log("Player " + player.name + " got $" + mobs[deadEntityIdentifier] + " for killing " + deadEntity.__identifier__);
 		//console.log(moneyOld, getBalance(player))
 	}
-	else{
-		system.executeCommand(`title "${player.name}" actionbar §fYou killed §6${deadEntity.__identifier__}`, () => {});
-		//console.log("Other mobs detected");
-	}
+	//else{
+	//	//system.executeCommand(`title "${player.name}" actionbar §fYou killed §6${deadEntity.__identifier__}`, () => {});
+	//	//console.log("Other mobs detected");
+	//}
 })
 console.log("mobReward.js loaded");
